1aabc93a161d2d5f2bce8ef777ede2f75218335b
Friannnnn <friangabrielmaravilla@gmail.com>
2024-10-01 14:28:09 +0800

    Ecom inital commit

---
+++ callback.php
@@ -0,0 +1,27 @@
+<?php
+use Google\Service\Oauth2;
+require_once 'vendor/autoload.php'; 
+
+session_start();
+
+$client = new Google_Client();
+$client->setClientId('320054654260-fcptfeujln15q5biepe21obl7q2bkvr3.apps.googleusercontent.com');
+$client->setClientSecret('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'); 
+$client->setRedirectUri('http://localhost/ecom/callback.php'); 
+
+if (isset($_GET['code'])) {
+    $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+    $_SESSION['access_token'] = $token;
+    $client->setAccessToken($token);
+    $oauth = new Oauth2($client);
+    $userInfo = $oauth->userinfo->get();
+
+    $_SESSION['email'] = $userInfo->email;
+    $_SESSION['name'] = $userInfo->name;
+    $_SESSION['picture'] = $userInfo->picture;
+
+    header('Location: profile.php');
+    exit();
+} else {
+    echo "No code received.";
+}
---
+++ profile.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['email'])) {
+    header('Location: login.php');
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Profile</title>
+</head>
+<body>
+    <p>Authenticated as <?= htmlspecialchars($_SESSION['email']); ?></p>
+    <h2>Welcome, <?= htmlspecialchars($_SESSION['name']); ?>!</h2>
+    <p>Email: <?= htmlspecialchars($_SESSION['email']); ?></p>
+    <p><img src="<?= htmlspecialchars($_SESSION['picture']); ?>" alt="Profile Picture"></p>
+
+    <a href="logout.php">Logout</a>
+</body>
+</html>
